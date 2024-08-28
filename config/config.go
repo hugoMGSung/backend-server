530371972b75ba6c6689a8df7f6c06823b5598ea
Hugo MG Sung <personar95@naver.com>
2024-08-28 10:56:49 +0900

    Config 만들기

--- config/config.go
+++ config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"os"
+
+	"github.com/naoina/toml"
+)
+
+// 설정 구조체
+type Config struct {
+	Server struct {
+		Port int64
+	}
+}
+
+func NewConfig(filePath string) *Config {
+	// var c *Config
+	c := new(Config)
+
+	if file, err := os.Open(filePath); err != nil {
+		panic(err)
+	} else if err = toml.NewDecoder(file).Decode(c); err != nil {
+		panic(err)
+	} else {
+		return c
+	}
+
+}
