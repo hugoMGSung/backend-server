45f013c773c151306bb05d6fd6be2c5633adabe7
Hugo MG Sung <personar95@naver.com>
2024-08-28 10:31:40 +0900

    Initial Commit
    최초 웹사이트 시작

---
+++ init/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("에러 발생!")
+		panic(err)
+	}
+
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello GoWeb!")
+}
